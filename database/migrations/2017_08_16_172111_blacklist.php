345107c4389bcaa48e921165166410f64819dd40
Troels Madsen <troels@madsen4.dk>
2017-12-08 12:31:18 +0100

    Updated Laravel to 5.2.45!

--- database/migrations/2017_08_16_172111_blacklist.php
+++ database/migrations/2017_08_16_172111_blacklist.php
@@ -17,7 +17,7 @@ class Blacklist extends Migration {
 			$table->increments('id');
 			$table->string('ip');
 			$table->text('reason');
-			$table->timestamps();
+			$table->timestamps()->useCurrent();
 			$table->softDeletes();
 		});
 	}
