119702e48006e59eadbf554b8fd256579ca1116b
Troels Madsen <troels@mydahlmadsen.dk>
2018-02-03 16:49:13 +0100

    Wign paths updated to english

--- resources/views/layout/menu.blade.php
+++ resources/views/layout/menu.blade.php
@@ -3,11 +3,11 @@
         <a href="{{ URL::to('/') }}"><img src="{{ asset( 'images/wign_logo_new.png' ) }}" alt="{{ __( 'common.Wign.logo' ) }}" class="wign logo-banner"></a>
     @endif
     <ul class="menu">
-        <li><a href="{{ URL::to('/opret') }}">{{ __( 'common.submit.sign' ) }}</a></li>
-        <li><a href="{{ URL::to('/requests') }}">{{ __( 'common.requested.sign' ) }}</a></li>
-        <li><a href="{{ URL::to('/seneste') }}">{{ __( 'common.latest.sign' ) }}</a></li>
-        <li><a href="{{ URL::to('/alle') }}">{{ __( 'common.all.sign' ) }}</a></li> {{-- Request fra Ragna. Bliver der indtil videre. Indtil der bliver til en alt for uoverskuelige liste --}}
-        <li><a href="{{ URL::to('/om') }}">{{ __( 'common.Wign.About' ) }}</a></li>
+        <li><a href="{{ URL::to( config('wign.urlPath.create')) }}">{{ __( 'common.submit.sign' ) }}</a></li>
+        <li><a href="{{ URL::to( config('wign.urlPath.request')) }}">{{ __( 'common.requested.sign' ) }}</a></li>
+        <li><a href="{{ URL::to( config('wign.urlPath.recent')) }}">{{ __( 'common.latest.sign' ) }}</a></li>
+        <li><a href="{{ URL::to( config('wign.urlPath.all')) }}">{{ __( 'common.all.sign' ) }}</a></li> {{-- Request fra Ragna. Bliver der indtil videre. Indtil der bliver til en alt for uoverskuelige liste --}}
+        <li><a href="{{ URL::to( config('wign.urlPath.about')) }}">{{ __( 'common.Wign.About' ) }}</a></li>
         <li><span class="brand">{{ config( 'wign.version' ) }}</span></li>
     </ul>
 </div>
