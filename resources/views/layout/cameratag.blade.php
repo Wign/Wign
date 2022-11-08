0906092d01a70e6f3c6d81ff7a4a065e9cf40cd8
Troels Madsen <tm@wign.dk>
2022-11-08 16:02:52 +0100

    Revert cameratag version. Dont want to mess with it!

--- resources/views/layout/cameratag.blade.php
+++ resources/views/layout/cameratag.blade.php
@@ -1,3 +1,3 @@
 <script src="/js/script.js" type="text/javascript"></script>
-<script src='//www.cameratag.com/v15/js/cameratag.min.js' type='text/javascript'></script>
-<link rel='stylesheet' href='//www.cameratag.com/v15/css/cameratag.css'>
+<script src='//cameratag.com/v12/js/cameratag.min.js' type='text/javascript'></script>
+<link rel='stylesheet' href='//cameratag.com/static/12/cameratag.css'>
